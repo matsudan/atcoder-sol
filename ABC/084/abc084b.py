8e49d5717690a5455189246fbd299792d2723229
matsudan <daaamatsun@gmail.com>
2019-11-02 23:29:15 +0900

    abc084b

--- ABC/084/abc084b.py
+++ ABC/084/abc084b.py
@@ -1,1 +1,8 @@
+a, b = map(int, input().split())
+s = input()
 
+if len(s) == a + b + 1 and s[a] == '-':
+    ss = s.split('-', 1)
+    print('Yes') if len(ss[0]) == a and ss[0].isdecimal and len(ss[1]) == b and ss[1].isdecimal() else print('No')
+else:
+    print('No')
