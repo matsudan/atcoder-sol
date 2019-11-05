66e2f76176f75b08823f10150b940d7a34f311c7
matsudan <daaamatsun@gmail.com>
2019-11-06 01:37:59 +0900

    change abc090b sol

--- ABC/090/abc090b.py
+++ ABC/090/abc090b.py
@@ -1,8 +1,4 @@
 a, b = map(int, input().split())
 
-cnt = 0
-for i in range(a, b+1):
-    if str(i) == str(i)[::-1]:
-        cnt += 1
-
-print(cnt)
+lst = [1 for i in range(a, b+1) if str(i) == str(i)[::-1]]
+print(sum(lst))
--- ABC/090/abc090b_diff.py
+++
@@ -1,4 +0,0 @@
-a, b = map(int, input().split())
-
-lst = [1 for i in range(a, b+1) if str(i) == str(i)[::-1]]
-print(sum(lst))
